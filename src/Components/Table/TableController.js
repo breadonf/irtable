e6f7a002a5a1bcbb302015fc6b3ec7836f8badfe
breadonf <fkc112006@hotmail.com>
2021-07-28 13:22:17 +0800

    start table
    start table

---
+++ src/Components/Table/TableController.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const TableController = () => {
+    return (
+        <div>
+            <button>Edit</button>
+            <button>Remove</button>
+            <label>Filter:</label>
+            <input type='text'></input>
+            <button>Search</button>
+            <button>Reset</button>
+        </div>
+    )
+}
+
+export default TableController
